763a375232798a820214a38c3d4546980c52d9b8
Ernesto Desanti <edesantis@ucenfotec.ac.cr>
2018-07-28 20:20:53 -0600

    Segunda refactorización

--- src/cribaeratostenes/CribaEratostenes.java
+++ src/cribaeratostenes/CribaEratostenes.java
@@ -1,18 +1,22 @@
 package cribaeratostenes;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class CribaEratostenes {
 
-	public static int[] generaCriba(int numMax){
-		int i,j;
+	public static int i, j;
+	public static int dim;
 	
+	public static int[] generaCriba(int numMax){
 		if (numMax >= 2){
-			int dim = numMax+1;
+			dim = numMax+1;
 			boolean[] primo = new boolean[dim];
 			
+			//Llena el arreglo de valores true
 			Arrays.fill(primo, Boolean.TRUE);
 			
+			//Define el 0 y el 1 como falsos
 			primo[0] = primo[1] = false;
 			
 			//Marca números no primos como false
@@ -25,21 +29,24 @@ public class CribaEratostenes {
 			}
 			
 			int[] primos = new int[numeroDePrimos(dim, primo)];
-			for (i = 0,j=0; i < dim; i++){
-				if (primo[i])
-					primos[j++] = 1;
-			}
 			
-			return primos;
+			return arregloPrimos(primos, primo);
 		} else {
 			return new int[0];
 		}
 		
 	}
 
+	private static int[] arregloPrimos(int[] pArregloFinal, boolean[] pArregloBool) {
+		for (i = 0,j=0; i < dim; i++){
+			if (pArregloBool[i])
+				pArregloFinal[j++] = 1;
+		}
+		return pArregloFinal;
+	}
+
 	//Cuenta número de primos
 	private static int numeroDePrimos(int dim, boolean[] primo) {
-		int i;
 		int cuenta = 0;
 		for (i = 0; i < dim; i++){
 			if (primo[i])
